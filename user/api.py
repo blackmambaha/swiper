8e0bcf2ee5b4dbdc596208b2e4d288f8bb9a4ab1
gits <123@qq.com>
2019-04-11 19:36:48 +0800

    1 add http render_json 2, add keys and errors 3,add yzx sms 4, add user login and auto create user

--- user/api.py
+++ user/api.py
@@ -1,5 +1,7 @@
+from django.http import JsonResponse
+
 from lib.sms import send_sms
-from lib.http import render_json
+from lib.http_sms import render_json
 from common import errors
 from django.core.cache import cache
 from common import keys
@@ -10,8 +12,10 @@ def sumbit_phone(request):
     """"获取短信验证码"""
     if not request.method == 'POST':
         return render_json('request method error',errors.REQUEST_ERROR)
+
     phone = request.POST.get('phone')
     result,msg = send_sms(phone)
+
     return render_json(msg)
 
 
@@ -21,15 +25,16 @@ def sumbit_vcode(request):
     # 判断是否是post请求
     if not request.method == 'POST':
         return render_json('request method error',errors.REQUEST_ERROR)
+    # 取到手机号
     phone = request.POST.get('phone')
-    # 取到缓存中的验证码
+    # 取到发到手机验证码
     vcode = request.POST.get('vcode')
     # 取到缓存中的验证码
     cache_vcode = cache.get(keys.VCODE_KEY %phone)
 
     # 对比验证码是否一致
     if vcode == cache_vcode:
-        user,_ = User.objects.get_or_create(phonenum=phone,nickname=phone)
+        user, _ = User.objects.get_or_create(phonenum=phone,nickname=phone)
 
         request.session['uid'] = user.id
         return render_json(user.to_string())
@@ -40,6 +45,12 @@ def sumbit_vcode(request):
 def get_profile(request):
     """获取个人资料"""
     pass
+    # uid = request.session.get('uid')
+    # user = User.objects.get(id=uid)
+    #
+    # profile = Profile.objects.get(id=uid)
+    #
+    # return render_json(profile.to_string())
 
 
 def set_profile(request):
